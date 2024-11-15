23dfc3a3ba7811618a170e54f58988505258182f
Amaryadav07 <anath2893007@gmail.com>
2024-11-15 22:31:30 +0530

    added code in loop section

---
+++ loop/power.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a=2,n=3,power=1;
+    for (int i = 1; i <=n; ++i)
+    {
+        power=power*a;
+        
+    }
+    cout<<"power is:"<<power;
+    
+}

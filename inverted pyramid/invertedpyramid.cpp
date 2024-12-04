9986c96cee86c554a06c85f741e4e437e1fa991c
Amaryadav07 <anath2893007@gmail.com>
2024-12-04 19:48:06 +0530

    added code of inverted pyramid

---
+++ inverted pyramid/invertedpyramid.cpp
@@ -0,0 +1,86 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+ 
+//  for (int r=1;r<=5;++r)
+//  {
+//     for (int i = 1; i <=9; i++)
+//     {
+//         if (i>=r && i<=10-r)
+//         {
+//             cout<<"*";
+//         }
+//         else
+//         {
+//             cout<<" ";
+//         }
+        
+        
+//     }
+//     cout<<endl;
+    
+//  }
+//  }
+
+//  #include <iostream>
+// using namespace std;
+// int main(){
+ 
+// int n;
+// cout<<"enter your number"<<endl;
+// cin>>n;
+
+// for (int r=1; r<=10; ++r)
+// {
+//    for (int c=1; c<=10; ++c)
+//    {
+//     if (c<=11-r)
+//     {
+//         cout<<"*";
+//     }
+//     else
+//     {
+//         cout<<" ";
+//     }
+    
+//    }
+//    cout<<endl;
+// }
+
+ 
+
+// }
+
+ #include <iostream>
+using namespace std;
+int main(){
+ 
+
+for (int r = 1;r<=5; r++)
+{
+    int a=r;
+ for (int c=1; c<=5; c++)
+ 
+ {
+    if (c<=r)
+    {
+       cout<<a; 
+        a++;
+    }
+     else
+     {
+        cout<<" ";
+     }
+   
+ }
+      cout<<endl; 
+      
+ }
+ 
+}
+
+
+
+
+ 
+
